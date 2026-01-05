b43b827306fd1af4962b4f3ab338c3e914073cb6
andrfh <andrew.solonov@gmail.com>
2026-01-05 14:43:08 +0300

    add: start to write a structure of project

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import router from './router.js';
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
